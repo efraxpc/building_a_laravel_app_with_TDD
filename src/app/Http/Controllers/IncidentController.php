b18622c9152f42ebc6557f3ca06792e6db0d315e
efraxpc <efraxpc@gmail.com>
2023-09-04 18:44:40 -0300

    remove unused edit method

--- src/app/Http/Controllers/IncidentController.php
+++ src/app/Http/Controllers/IncidentController.php
@@ -64,20 +64,9 @@ class IncidentController extends Controller
     {
         $incident = Incident::find($id);
 
-        return view('admin.incidents.show', [
+        return view('admin.incidents.edit', [
             'incident' => $incident
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
